355dca694110da48caca6d9fc62be3d7035b3e3c
Alexander Turovski <turovskialexandet@gmail.com>
2026-01-27 11:17:46 +0100

    :)

--- Car.java
+++ Car.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 
-public class Car {
+public class Car implements Movable {
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model
+    public Point coordinates; // Car coordinates
+    private int state = 1;
 
     // getter-function that returns the number of doors a car has
     public int getNrDoors(){
@@ -42,6 +44,35 @@ public class Car {
         currentSpeed = 0;
     }
 
-
+    // interface
+    // potential changes are expected for these methods
+    public void move() {
+        switch(state){
+            case 1:
+                coordinates.y += currentSpeed;
+            case 2:
+                coordinates.x += currentSpeed;
+            case 3:
+                coordinates.y -= currentSpeed;
+            case 4:
+                coordinates.x -= currentSpeed;
+        }
+    }
+    public void turnLeft() {
+        if(state < 1){
+            state = 4;
+        }
+        else{
+            state -= 1;
+        }
+    }
+    public void turnRight() {
+        if (state > 4){
+            state = 1;
+        }
+        else {
+            state += 1;
+        }
+    }
 
 }
