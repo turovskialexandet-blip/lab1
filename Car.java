e5b30594484c52d5a550dad55251e12d540a7205
Alexander Turovski <turovskialexandet@gmail.com>
2026-01-27 10:13:01 +0100

    Created Subclass Car!

--- Car.java
+++ Car.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 
 public class Car {
-    public String hello;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
